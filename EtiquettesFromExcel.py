c4e419f45cf9832845fd3ae70be4e2420d7c1809
Jean-LoupIsReal <jean.loupca@hotmail.fr>
2025-07-02 15:32:05 -0400

    Rendre code plus modulaire

--- EtiquettesFromExcel.py
+++ EtiquettesFromExcel.py
@@ -5,6 +5,7 @@ import os
 # Liste des paquets nécessaires
 packages = ["reportlab", "pandas", "numpy", "openpyxl"]
 
+# Installation des packages non installé sur l'ordinateur
 for package in packages:
     try:
         __import__(package)
@@ -13,6 +14,7 @@ for package in packages:
         #print(f"Installation de '{package}'...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
         print("Instalation terminé, vous etes prets à utiliser le programme.")
+
 print("Selectionnez votre fichier Excel pour les etiquettes.")
 import pandas as pd
 from datetime import datetime, date
@@ -29,6 +31,7 @@ class EtiquettesFromExcel:
     def creer_pdf(self): 
         # Déclarer les variable des données
         df_inventaire = pd.read_csv(self.CSV_SERVEX_PATH)
+        #choisir le fichier à imprimer
         df_impression = pd.read_excel(choisir_fichier_excel())
 
         #location = self.ask_location()
@@ -45,7 +48,7 @@ class EtiquettesFromExcel:
         generateur_etiquettes.generer_pdf_petite_etiquettes(df_impression.groupby(["No"]).first(), os.path.join(self.BASE_DIR, f"etiquettes-{now}.pdf"), True)
 
  
-# Exemple d’utilisation
+# Execution du script
 if __name__ == "__main__":
     e = EtiquettesFromExcel()
     try:
--- GenerateurEtiquettes.py
+++ GenerateurEtiquettes.py
@@ -34,6 +34,11 @@ class GenerateurEtiquettes:
                 fontSize=8
             )
         self.largeur_page, self.hauteur_page = dimentions_page
+        with open('dimEtiquette.json', 'r') as file:
+            self.type_etiquettes = json.load(file)
+            self.petite_etiquettes = self.type_etiquettes["petite_etiquette"]
+            self.moyenne_etiquettes = self.type_etiquettes["moyenne_etiquette"]
+            self.grande_etiquettes = self.type_etiquettes["grande_etiquette"]
     
     def dessiner_grille(self, c, hauteur_etiquette, largeur_etiquette, nb_par_ligne, nb_par_colonne):
         c.setDash(6, 3)
@@ -46,12 +51,15 @@ class GenerateurEtiquettes:
             c.line(self.marge_externe, y, self.largeur_page - self.marge_externe, y)
 
     # Imprime le
-    def print_gros_prix(self, canva, prix, emplacement_x, emplacement_y):
-        
+    def print_gros_prix(self, canvas, prix, type_etiquette):
+        # Transforme le prix en int pour enlever les décimales
+        # Calcul emplacement prix
+        # Calcul emplacement cents
+        # Imprime prix et cents
         pass
 
 
-    def generer_grande_etiquette(self, data, fichier_pdf="etiquettes.pdf"):
+    def generer_pdf_grande_etiquette(self, data, fichier_pdf="etiquettes.pdf"):
         c = canvas.Canvas(fichier_pdf, pagesize=(self.largeur_page, self.hauteur_page))
         largeur_section1 = 100 * mm
         hauteur_section1 = 35 * mm
@@ -67,10 +75,17 @@ class GenerateurEtiquettes:
         c = canvas.Canvas(fichier_pdf, pagesize=(self.largeur_page, self.hauteur_page))
 
         # Marges et dimensions
-        nb_par_ligne = 4
-        nb_par_colonne = 8  # Ajustable selon hauteur
-        largeur_etiquette = (self.largeur_page - 2 * self.marge_externe) / nb_par_ligne
-        hauteur_etiquette = 33 * mm  # Hauteur fixe
+        largeur_etiquette = self.petite_etiquettes["largeur"] * mm
+        hauteur_etiquette = self.petite_etiquettes["hauteur"] * mm
+        nb_par_ligne = int((self.hauteur_page - 2 * self.marge_externe) / largeur_etiquette)
+        nb_par_colonne = int((self.hauteur_page - 2 * self.marge_externe) / hauteur_etiquette) # Ajustable selon hauteur
+
+        h_section_prix = self.petite_etiquettes["h_section_prix"] * mm
+        h_section_desc = self.petite_etiquettes["h_section_desc"] * mm
+        h_section_code = self.petite_etiquettes["h_section_code"] * mm
+
+        marge_interne_x = self.petite_etiquettes["marge_interne_x"]
+        marge_interne_y = self.petite_etiquettes["marge_interne_y"]
 
         nb_par_page = nb_par_ligne * nb_par_colonne
         
@@ -91,61 +106,59 @@ class GenerateurEtiquettes:
 
             x = self.marge_externe + col * largeur_etiquette
             y = self.hauteur_page - self.marge_externe - (lig + 1) * hauteur_etiquette
-            # Déclare Hauteur
-            if(inverse):    
-                h_produit = y + 4*mm + 2 + 6
-                h_ligne2 = h_produit + 4 * mm - 1
-                h_desc = h_ligne2 + 1 * mm + 17
-                h_ligne1 = h_ligne2 + 13 * mm
-                h_prix = y + hauteur_etiquette - 27
-
+            h_code_bar = self.petite_etiquettes["h_code_bar"] * mm
+            # Déclare Hauteur (du bas au haut)
+            
+            h_produit = y + h_code_bar + (h_section_code - h_code_bar)/2
+            h_ligne_bas = y + h_section_code
+            if(inverse):
+                h_prix = h_ligne_bas + marge_interne_y
+                h_ligne_haut = h_ligne_bas + h_section_prix
+                h_desc = h_ligne_haut + h_section_desc/2
             else:    
-                h_produit = y + 4*mm + 2 + 6
-                h_ligne2 = h_produit + 4 * mm - 1
-                h_desc = h_ligne2 + 1 * mm + 17
-                h_ligne1 = h_ligne2 + 13 * mm
-                h_prix = y + hauteur_etiquette - 27
+                h_desc = h_ligne_bas + h_section_desc/2
+                h_ligne_haut = h_ligne_bas + h_section_desc
+                h_prix = h_ligne_haut + marge_interne_y
 
             # Prix (très gros)
             c.setFont("Helvetica-Bold", 30)
             c.drawCentredString(x + largeur_etiquette / 2, h_prix, f"{row[1].Coûtant:.2f} $")
             
-            # Ligne1
+            # Ligne haut
             c.setDash(1,0)
             c.setStrokeColor(colors.gray)
-            c.line(x, h_ligne1, x+largeur_etiquette, h_ligne1)
+            c.line(x, h_ligne_haut, x+largeur_etiquette, h_ligne_haut)
 
+            # Declare la description
+            p = Paragraph(str(row[1].Description), self.style_description)
+            p.wrapOn(c, largeur_etiquette - marge_interne_x * 2, (h_desc - 2 * marge_interne_y) * 3/4 )
+            
             if not np.isnan(row[1].Ecofrais):
-                # Description
-                p = Paragraph(str(row[1].Description), self.style_description)
-                p.wrapOn(c, largeur_etiquette - 10, 20 * mm)
-                p.drawOn(c, x + 5, h_desc - p.height/2)
-            else:
-                h_desc -= 4
-                # Description
-                p = Paragraph(str(row[1].Description), self.style_description)
-                p.wrapOn(c, largeur_etiquette - 10, 20 * mm)
-                p.drawOn(c, x + 5, h_desc - p.height/2)
-                # Ecofrais
+                # Declare les ecofrais
                 combined_string = "Inclus " + str(row[1].Ecofrais) + "$ d'écofrais"
                 pEf = Paragraph(combined_string, self.style_description)
-                pEf.wrapOn(c, largeur_etiquette - 10, 20 * mm)
-                pEf.drawOn(c, x + 5, h_desc + p.height/2)
+                pEf.wrapOn(c, largeur_etiquette - marge_interne_x * 2, 20 * mm)
+                h_desc -= pEf.height/2
+                # Dessine les ecofrais
+                pEf.drawOn(c, x + marge_interne_x, h_desc + p.height/2)
+            # Dessine la description
+            p.drawOn(c, x + marge_interne_x, h_desc - p.height/2)
 
-            # Ligne 2
-            c.line(x, h_ligne2, x + largeur_etiquette, h_ligne2)
+            # Ligne bas
+            c.line(x, h_ligne_bas, x + largeur_etiquette, h_ligne_bas)
 
-            # No produit
+            # Code du produit
             p2 = Paragraph(str(row[0]),self.style_produit)
-            w = largeur_etiquette - 10
-            h = 12
+            h = h_section_code - h_code_bar - marge_interne_y * 2
+            w = largeur_etiquette - marge_interne_x * 2
+            # S'assure que le code reste dans le frame
             k = KeepInFrame(w, h, [p2], mode='shrink')
             k.wrapOn(c, w, h)
-            k.drawOn(c, x + 5, h_produit)
+            k.drawOn(c, x + marge_interne_x, h_produit)
 
             # Code-barres
             if pd.notna(row[0]):
-                barcode = code128.Code128(str(row[0]), barHeight= 4 * mm, humanReadable=False)
+                barcode = code128.Code128(str(row[0]), barHeight= h_code_bar, humanReadable=False)
                 barcode.drawOn(c, x + (largeur_etiquette - barcode.width) / 2, y + 1)
             i += 1
         try:
